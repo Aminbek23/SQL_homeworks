af93acbe3185a2b095707f4ae0eae7ae84861766
Aminbek23 <aminbekqudratov23@gmail.com>
2025-08-11 10:13:08 +0500

    Update and rename lesson-1/hometask1 to lesson-1/homework/hometask1.sql

---
+++ lesson-1/homework/hometask1.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE READBOOKSDB; -- database yaratish
+
+USE READBOOKSDB; -- foydalanishi 
+CREATE TABLE Books  (  
+    Books_Id INT ,
+    Name VARCHAR(30) ,
+    Author VARCHAR(30),
+    Genre VARCHAR(20),
+    Status VARCHAR(30)
+);
+
